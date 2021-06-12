90fa3d6015a1481610ce7946efeb09fbf674009d
Bernhard <1688588+berndsed@users.noreply.github.com>
2021-06-12 11:34:56 +0200

    Combine closely related tests of BatchSpec into one test method

--- src/test/groovy/de/kieseltaucher/duplex/foreverybody/core/BatchSpec.groovy
+++ src/test/groovy/de/kieseltaucher/duplex/foreverybody/core/BatchSpec.groovy
@@ -11,25 +11,20 @@ class BatchSpec extends Specification {
         batch == []
     }
 
-    def 'empty batch does not change'() {
+    def 'batches in correct order do not change'() {
         when:
         batch.simplex2Duplex()
         then:
         batch == []
-    }
 
-    def 'batch with one page does not change'() {
-        given:
+        and:
         batch.add('Page 1')
         when:
         batch.simplex2Duplex()
         then:
         batch == ['Page 1']
-    }
 
-    def 'batch with two pages does not change'() {
-        given:
-        batch.add('Page 1')
+        and:
         batch.add('Page 2')
         when:
         batch.simplex2Duplex()
