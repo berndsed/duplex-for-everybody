8ece11ff48d2f927cedb284f5a65da41cb998dab
Bernhard <1688588+berndsed@users.noreply.github.com>
2021-06-12 12:03:08 +0200

    Optimize sort algorithm
    Make one move step less when the number of pages is even.

--- src/main/groovy/de/kieseltaucher/duplex/foreverybody/core/Batch.groovy
+++ src/main/groovy/de/kieseltaucher/duplex/foreverybody/core/Batch.groovy
@@ -10,14 +10,7 @@ trait Batch<T> {
         final int size = size()
         final boolean odd = size % 2 == 1
 
-        /* Given there is a scan with four pages:
-         * [Page 1, Page 3, Page 4, Page 2]
-         *
-         * The index of the first even page is 2 (an even number).
-         *
-         * The algorithm needs to move the page at index 2 (Page 4) to index 2.
-         * Next the algorithm will move the page at index 3 to index 1.
-         *
+        /*
          * Given there is a scan with five pages (an odd number):
          *
          * [Page 1, Page 3, Page 5, Page 4, Page 2]
@@ -26,12 +19,24 @@ trait Batch<T> {
          *
          * The algorithm needs to move the page at index 3 (Page 4) to index 2.
          * Next the algorithm needs move the page at index 4 to index 1.
+         *
+         * Given there is a scan with four pages (an even number):
+         * [Page 1, Page 3, Page 4, Page 2]
+         *
+         * The index of the first even page is 2.
+         *
+         * The algorithm does not needs to move the page at index 2 (Page 4).
+         * It is the last page, and may stay after Page 3.
+         *
+         * But it needs to move all subsequent pages.
+         * In this example it needs to move the page at index 3 (Page 2) to index 1 (after Page 1).
+         *
          */
 
         final int idxBackPages = (size + (odd ? 1 : 0)) / 2
 
-        int insertIdx = idxBackPages - (odd ? 1 : 0)
-        int removeIdx = idxBackPages
+        int insertIdx = idxBackPages - 1
+        int removeIdx = idxBackPages + (odd ? 0 : 1)
         while(removeIdx < size) {
             def evenPage = remove(removeIdx)
             add(insertIdx, evenPage)
