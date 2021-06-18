7c3eb312a5980f425ac50932b30a9699e6cec2db
Bernhard <1688588+berndsed@users.noreply.github.com>
2021-06-18 18:42:41 +0200

    Extract a method in BatchService loading the data

--- src/main/groovy/de/kieseltaucher/duplex/foreverybody/service/BatchService.groovy
+++ src/main/groovy/de/kieseltaucher/duplex/foreverybody/service/BatchService.groovy
@@ -3,16 +3,18 @@ package de.kieseltaucher.duplex.foreverybody.service
 class BatchService {
 
     void simplex2Duplex(InputStream simplex, OutputStream duplex) {
-        PDFBoxBatch batch
-        try {
-            batch = PDFBoxBatch.load(simplex)
-        } finally {
-            simplex.close()
-        }
+        PDFBoxBatch batch = loadBatch(simplex)
         batch.simplex2Duplex()
 
         batch.save(duplex)
         duplex.flush()
     }
 
+    private PDFBoxBatch loadBatch(InputStream data) {
+        try {
+            return PDFBoxBatch.load(data)
+        } finally {
+            data.close()
+        }
+    }
 }
